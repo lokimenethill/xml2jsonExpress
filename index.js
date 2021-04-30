04ccc0e3a628391f93f883592dea233332947b4c
lokimenethill <xdjtrancemetal@hotmail.com>
2021-04-30 18:14:32 -0500

    Upgrade

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var parser = require('xml2json');
 var fs = require('fs');
 var xml = fs.readFileSync('a.eaf', 'utf8');
 
+
 //var xml = "<foo attr=\"value\">bar</foo>";
 //console.log("input -> %s", xml)
  
@@ -18,11 +19,35 @@ var xml = parser.toXml(json);
 console.log("back to xml -> %s", xml)*/
 
 app.get('/', (req, res) => {
-    var json = parser.toJson(xml);
-console.log("to json -> %s", json);
-    res.send(json)
+    var json = JSON.parse(parser.toJson(xml));
+//console.log("to json -> %s", json);
+    res.json(json)
   })
   
+app.get('/compare/dataArr', (req, res) => {
+  var compare = fs.readFileSync('dataArr.json');
+  var json = JSON.parse(compare);
+//console.log("to json -> %s", json);
+  res.json(json)
+})
+app.get('/compare/lineTimeArr', (req, res) => {
+  var compare = fs.readFileSync('lineTimeArr.json');
+  var json = JSON.parse(compare);
+//console.log("to json -> %s", json);
+  res.json(json)
+})
+app.get('/compare/timeArr', (req, res) => {
+  var compare = fs.readFileSync('timeArr.json');
+  var json = JSON.parse(compare);
+//console.log("to json -> %s", json);
+  res.json(json)
+})
+app.get('/compare/timeslotArr', (req, res) => {
+  var compare = fs.readFileSync('timeslotArr.json');
+  var json = JSON.parse(compare);
+//console.log("to json -> %s", json);
+  res.json(json)
+})
   app.listen(8080, () => {
     console.log(`Example app listening at http://localhost:8080`)
   })

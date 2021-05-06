9543b65094451dfdc80662e0b205f9248e628e19
rivalcoba <ivan.rivalcoba@gmail.com>
2021-05-06 10:21:07 -0500

    Improving organization

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const eafTools = require('./helpers/eafTools');
+const eafTools = require('../helpers/eafTools');
 var parser = require('xml2json');
 var fs = require('fs');
-const { Console } = require('console');
+const appRoot = require('app-root-path').path;
 
 // Reading the file
 let eafXml = fs.readFileSync('./originals/a01.eaf', 'utf8');
 var eafJs = JSON.parse(parser.toJson(eafXml));
-
 // Adding static files
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(appRoot,'public')));
 
 app.get('/res/timeslotArr', (req, res) => {
   let timeslotArr = eafTools.getTimeSlotArray(eafJs);
@@ -33,7 +32,7 @@ app.get('/res/tierArr', (req, res) => {
   res.json(tierArr);
 });
 
-app.get('/', (req, res) => {
+app.get('/eaf', (req, res) => {
   
   eafJs = JSON.parse(parser.toJson(eafXml));
   //console.log("to json -> %s", json);
@@ -41,22 +40,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/org/dataArr', (req, res) => {
-  let dataArr = require('./originals/dataArr.json')
+  let dataArr = require('../originals/dataArr.json')
   res.json(dataArr);
 });
 
 app.get('/org/tierArr', (req, res) => {
-  let tierArr = require('./originals/tierArr.json')
+  let tierArr = require('../originals/tierArr.json')
   res.json(tierArr);
 });
 
 app.get('/org/timeslotArr', (req, res) => {
-  let timeslotArr = require('./originals/timeslotArr.json')
+  let timeslotArr = require('../originals/timeslotArr.json')
   res.json(timeslotArr);
 });
 
 app.get('/org/lineTimeArr', (req, res) => {
-  let lineTimeArr = require('./originals/lintetimeArr.json')
+  let lineTimeArr = require('../originals/lintetimeArr.json')
   res.json(lineTimeArr);
 });
 

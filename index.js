b365b8c33b8df8a14fc92bb8c499ef1018a3503c
rivalcoba <ivan.rivalcoba@gmail.com>
2021-05-06 10:02:33 -0500

    Organizing Project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const eafTools = require('./eafTools');
+const eafTools = require('./helpers/eafTools');
 var parser = require('xml2json');
 var fs = require('fs');
 const { Console } = require('console');
